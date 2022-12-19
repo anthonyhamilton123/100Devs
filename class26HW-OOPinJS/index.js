aaf18f145709dbea66d08f7cdd886ea75a3c625c
Anthony Hamilton <anthonynhamilton@gmail.com>
2022-12-19 14:14:33 -0600

    "updated 12.19.22"

---
+++ class26HW-OOPinJS/index.js
@@ -0,0 +1,19 @@
+
+// sw constructor
+//duration
+//reset
+//start
+//stop
+
+let SW = function(){
+    this.duration = 0,
+    this.start = function(){
+
+    }
+    this.stop = function(){
+
+    }
+    this.reset = function(){
+        this.duration = 0
+    }
+}
